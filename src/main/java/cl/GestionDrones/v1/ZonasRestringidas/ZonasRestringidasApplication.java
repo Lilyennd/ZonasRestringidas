9e4b8aee6c2cf9f329cda1e229c7029e3a3bc31b
Lilyennd <victoriariffo21@gmail.com>
2026-05-18 11:34:53 -0400

    zr1

---
+++ src/main/java/cl/GestionDrones/v1/ZonasRestringidas/ZonasRestringidasApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.ZonasRestringidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonasRestringidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonasRestringidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/GestionDrones/v1/ZonasRestringidas/ZonasRestringidasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.ZonasRestringidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonasRestringidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
